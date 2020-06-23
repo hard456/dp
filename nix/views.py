a9075aaf81ff63b8475e2bb16252ee6ae46c2977
hard456 <janpalcut@seznam.cz>
2020-06-23 16:55:17 +0200

    zkouška směrování + základní šablona

--- nix/views.py
+++ nix/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'nix/index.html')
+
+
+def testik(request):
+    return render(request, 'nix/test.html')
+
+
+def error_404(request, exception):
+    data = {}
+    return render(request, '404.html', data)
+
+
+def error_500(request):
+    data = {}
+    return render(request, '500.html', data)
+
+
+def error_403(request, exception):
+    data = {}
+    return render(request, '403.html', data)
+
+
+def error_400(request, exception):
+    data = {}
+    return render(request, '400.html', data)
