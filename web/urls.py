d7dbb61e2064a47fb817c1bd5c2458764464e4cd
hard456 <janpalcut@seznam.cz>
2020-06-23 16:52:38 +0200

    new module name + configuration

--- web/urls.py
+++ web/urls.py
@@ -1,4 +1,4 @@
-"""dp URL Configuration
+"""web URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("nix.urls")),
 ]
+
+handler404 = "nix.views.error_404"
+handler500 = "nix.views.error_500"
+handler403 = "nix.views.error_403"
+handler400 = "nix.views.error_400"
