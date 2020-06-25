3ddb4fea39bdcbc591125270f74301d5b43e8e1a
hard456 <janpalcut@seznam.cz>
2020-06-25 23:32:53 +0200

    otevření NIX souboru + rozdělaný file upload

--- nix/views.py
+++ nix/views.py
@@ -1,3 +1,7 @@
+import os
+
+import nixio as nix
+from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,7 +11,26 @@ def index(request):
     return render(request, 'nix/index.html')
 
 
+def simple_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+    #     return render(request, 'core/simple_upload.html', {
+    #         'uploaded_file_url': uploaded_file_url
+    #     })
+    # return render(request, 'core/simple_upload.html')
+        return render(request, 'nix/test.html')
+
+
 def testik(request):
+    module_dir = os.path.dirname(__file__)  # get current directory
+    file_path = os.path.join(module_dir, '../experiments/file_create_example.h5')
+    nix_file = nix.File.open(file_path, nix.FileMode.ReadOnly)
+
+    print(nix_file.format, nix_file.version, nix_file.created_at)
+    nix_file.close()
     return render(request, 'nix/test.html')
 
 
