8786c7ca852d377927bffb22251ceffe1c003849
hard456 <janpalcut@seznam.cz>
2020-06-28 04:55:35 +0200

    overeni unikatniho id experimentu

--- nix/views.py
+++ nix/views.py
@@ -4,6 +4,7 @@ import nixio as nix
 import uuid
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render, redirect
+from django.conf import settings
 
 
 def index(request):
@@ -20,12 +21,22 @@ def upload_experiment(request):
     if request.method == 'POST' and request.FILES.get('myfile', False):
         file = request.FILES['myfile']
         file_name = file.name.lower()
+
+        # check file extension
         if not file_name.endswith('.nix') and not file_name.endswith('.h5'):
             return render(request, 'nix/index.html', {
                 'error_message': "The file can only have a .nix or .h5 extension."
             })
-        experiment_id = str(uuid.uuid1().int)
+
         fs = FileSystemStorage()
+
+        # check unique experiment id
+        while True:
+            experiment_id = str(uuid.uuid1().int)
+            print(settings.MEDIA_URL + 'experiments/' + '34990128788807444172295489484962121020' + '/')
+            if not fs.exists('experiments/' + experiment_id + '/'):
+                break
+
         fs.save('experiments/' + experiment_id + '/' + file_name, file)
         return redirect('/experiment/' + experiment_id + '/file')
     return render(request, 'nix/index.html', {
@@ -39,6 +50,7 @@ def testik(request):
     nix_file = nix.File.open(file_path, nix.FileMode.ReadOnly)
 
     print(nix_file.format, nix_file.version, nix_file.created_at)
+    sections = nix_file.pprint()
     nix_file.close()
     return render(request, 'nix/test.html')
 
